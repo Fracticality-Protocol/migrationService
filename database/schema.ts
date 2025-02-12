2d51164ae9b1233c4b06c836bc7ad6be659c40a5
Jose Herrera <josemiguelherrera@protonmail.com>
2025-02-12 18:10:09 -0500

    format for easier reading (for jose), we need to get the same styling going.

--- database/schema.ts
+++ database/schema.ts
@@ -1,18 +1,21 @@
-import { env } from '../env'
+import { env } from "../env";
 
-import { integer, text, boolean, pgSchema } from 'drizzle-orm/pg-core'
+import { integer, text, boolean, pgSchema } from "drizzle-orm/pg-core";
 
-export const schema = pgSchema(env.DB_SCHEMA || 'test')
+export const schema = pgSchema(env.DB_SCHEMA || "test");
 
-export const fractalityTokenMigrations = schema.table('fractality_token_migrations', {
-  transactionHash: text('transaction_hash').primaryKey(),
-  migrationContractAddress: text('migration_contract_address'),
-  eventName: text('event_name'),
-  caller: text('caller'),
-  migrationAddress: text('migration_address'),
-  amount: text('amount'),
-  foundAt: text('found_at'),
-  status: text('status'),
-  migratedAt: text('migrated_at'),
-  migratedAmount: text('migrated_amount')
-})
+export const fractalityTokenMigrations = schema.table(
+  "fractality_token_migrations",
+  {
+    transactionHash: text("transaction_hash").primaryKey(),
+    migrationContractAddress: text("migration_contract_address"),
+    eventName: text("event_name"),
+    caller: text("caller"),
+    migrationAddress: text("migration_address"),
+    amount: text("amount"),
+    foundAt: text("found_at"),
+    status: text("status"),
+    migratedAt: text("migrated_at"),
+    migratedAmount: text("migrated_amount"),
+  }
+);
